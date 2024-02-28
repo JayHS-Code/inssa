5e2bfe456de45d302e7af5a00a39dea75dd1937a
Jay <hugevory1999@naver.com>
2024-02-28 15:11:01 +0900

    header, bottom 메뉴 css 수정 및 아이콘 추가

--- src/components/bottomMenu.tsx
+++ src/components/bottomMenu.tsx
@@ -1,24 +1,70 @@
 import useUser from "@/libs/client/useUser";
 import Link from "next/link";
+import {
+  IconChatBubble,
+  IconChatBubbleSolid,
+  IconFilm,
+  IconFilmSolid,
+  IconHome,
+  IconHomeSolid,
+  IconVideo,
+  IconVideoSolid,
+} from "./svg";
+import { useRouter } from "next/router";
 
 export default function BottomMenu() {
   const { user } = useUser();
+  const router = useRouter();
+  console.log(router.asPath);
   return (
-    <div className="bg-white border-t border-black fixed w-full max-w-3xl left-2/4 -translate-x-2/4 bottom-0 flex justify-around z-0">
+    <div className="bg-white py-2 border-t border-black fixed w-full max-w-3xl left-2/4 -translate-x-2/4 bottom-0 flex items-center justify-around z-0">
       <Link href="/">
-        <div>Home</div>
+        <div>
+          {router?.asPath === "/" ? (
+            <IconHomeSolid cls={"w-7 h-7"} />
+          ) : (
+            <IconHome cls={"w-7 h-7"} />
+          )}
+        </div>
       </Link>
       <Link href="/v">
-        <div>video</div>
+        <div>
+          {router?.asPath === "/v" ? (
+            <IconFilmSolid cls={"w-7 h-7"} />
+          ) : (
+            <IconFilm cls={"w-7 h-7"} />
+          )}
+        </div>
       </Link>
       <Link href="/live">
-        <div>Live</div>
+        <div>
+          {router?.asPath === "/live" ? (
+            <IconVideoSolid cls={"w-7 h-7"} />
+          ) : (
+            <IconVideo cls={"w-7 h-7"} />
+          )}
+        </div>
       </Link>
       <Link href="/direct">
-        <div>Direct</div>
+        <div>
+          {router?.asPath === "/direct" ? (
+            <IconChatBubbleSolid cls={"w-7 h-7"} />
+          ) : (
+            <IconChatBubble cls={"w-7 h-7"} />
+          )}
+        </div>
       </Link>
       <Link href={`/profile/${user?.nickname}`}>
-        <div>Profile</div>
+        <div>
+          <img
+            className={`w-7 h-7 rounded-full ${
+              router?.asPath === `/profile/${user?.nickname}`
+                ? "border-solid border-2 border-white"
+                : null
+            }`}
+            src={user?.avatar ? user?.avatar : "/empty.png"}
+          />
+        </div>
       </Link>
     </div>
   );
--- src/components/headerMenu.tsx
+++ src/components/headerMenu.tsx
@@ -11,8 +11,10 @@ export default function HeaderMenu({ title }: TitleType) {
       <Head>
         <title>홈 - </title>
       </Head>
-      <div className="bg-white fixed px-4 h-12 w-full max-w-3xl top-0 left-2/4 -translate-x-2/4 flex justify-between items-center border-b">
-        <Link href="/">inssa</Link>
+      <div className="bg-white fixed px-4 h-14 w-full max-w-3xl top-0 left-2/4 -translate-x-2/4 flex justify-between items-center border-b">
+        <Link className="font-bold text-xl" href="/">
+          inssa
+        </Link>
         <Link href="/upload">업로드</Link>
       </div>
     </div>
